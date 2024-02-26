1389e02c1a05a77bac1ea2a7fa81b73852aaa00d
Carolina Estrada <141278521+carolinaestm@users.noreply.github.com>
2024-02-25 22:55:32 -0600

    Update guessinggame.cpp

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
 #include <cctype>
+#include <ctime>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-  
-  //declarations
-  int choice;
+  // declarations
+  int choice; //user input from menu
   int points = 100;
   enum choiceNum {
     displayLeft = 1,
@@ -15,158 +16,171 @@ int main() {
     change = 4,
     exit = 5
   };
-  bool valid;
+  bool validName;
   string name;
   // Ask for their name and store it
   cout << "What is your name? " << endl;
   getline(cin, name);
 
-  //making sure name is valid
-  do{
-    valid = true;
-    for(int i = 0; i < name.length(); i++){
-      if(!(isalpha(name[i])) && name[i] != ' '){
-        valid = false;
-      } 
-    }
-    if(!valid){
-      // edited to make sure the user knows it can only be letters and spaces. Then record new name.
-      cout << "Name can only contain letters and spaces." << endl << "Please enter a valid name: " << endl;
-      getline(cin, name);
-      valid = false;
-    }
-      // check for name length and set the first  letter to uppercase. change senior to Sr. and junior to Jr. Set valid to true. Do while until correct name is implemented.
-      else{
-        for(int x = 0; x < name.length(); x++){
-          name[x] = tolower(name[x]);
-        }
-        name[0] = toupper(name[0]);
-        if(name.find("senior") != string::npos){
-          name.replace(name.find("senior"), 6, "Sr");     
-        }
-        if(name.find("junior") != string::npos){
-          name.replace(name.find("junior"), 6, "Jr");     
-        }
-        int space = name.find(' ');
-        name[space + 1] = toupper(name[space + 1]);
-        cout << "Welcome " << name << endl;
-        valid = true;
+  // making sure name is valid
+  do {
+    validName = true;
+    //every character in name must be a letter or space
+    for (int i = 0; i < name.length(); i++) {
+      if (!(isalpha(name[i])) && name[i] != ' ') {
+        validName = false;
+        break;
       }
     }
-  while(!valid);
+    if (!validName) {
+      //getting user to reenter name if contain invalid characters
+      cout << "Name can only contain letters and spaces." << endl;
+      cout << "Please enter a valid name: " << endl;
+      getline(cin, name);
+      validName = false;
+    }
+    // adding uppercases where need be and including the name suffix
+      else {
+        //lowercase every character in name
+        for (int x = 0; x < name.length(); x++) {
+         name[x] = tolower(name[x]);
+       }
+        //uppercase first character
+        name[0] = toupper(name[0]);
+        //adding suffix
+       if (name.find("senior") != string::npos) {
+        name.replace(name.find("senior") - 1, 7, ", Sr");
+       }
+        //adding suffix
+       if (name.find("junior") != string::npos) {
+          name.replace(name.find("junior") - 1, 7, ", Jr");
+       }
+       //uppercase everywhere after space
+       for(int y = 0; y < name.length(); y++){
+        if(name[y] == ' ') {
+           name[y + 1] = toupper(name[y + 1]);
+         }
+       }
+       cout << "Welcome " << name << endl;
+        //breaking loop if name is valid
+        validName = true;
+     }
+   } while (!validName);
 
-  //getting random numbers for left and right and keeping left less than right from 150-250 inclusive.
+  // getting random numbers for left and right and keeping left less than right
+  // from 150-250 inclusive.
   srand(time(NULL));
   int left = rand() % 101 + 150;
   int right = rand() % 101 + 150;
-  while(left >= right){
-   left = rand() % 101 + 150;
+  while (left >= right) {
+    left = rand() % 101 + 150;
   }
-  //initializing displaying numbers that the user sees
+  
+  // initializing display numbers that the user sees
   int num1 = -1;
   int num2 = -1;
   cout << num1 << "  " << num2 << endl << endl;
-
-  //the game loop
   
-  //the menu
+  // the game loop
   do {
-  cout << "Menu: " << endl;
-  cout << "1. Display Left Number" << endl;
-  cout << "2. Display Right Number" << endl;
-  cout << "3. Guess a number in between" << endl;
-  cout << "4. Change numbers" << endl;
-  cout << "5. Exit" << endl;
-  cout << "What do you want to do? " << endl;
-  cin >> choice;
-  //making choice a valid input for the enum
-  //making strings invalid
-  bool isInt = true;
-  if(isdigit(choice)){
-   isInt = true;
+    // the menu
+    cout << "Menu: " << endl;
+    cout << "1. Display Left Number" << endl;
+    cout << "2. Display Right Number" << endl;
+    cout << "3. Guess a number in between" << endl;
+    cout << "4. Change numbers" << endl;
+    cout << "5. Exit" << endl;
+    cout << "What do you want to do? " << endl;
+    
+    //making choice fit neccesary parameters and clearing invalid inputs
+    while (!(cin >> choice) || choice < 1 || choice > 5) {
+      cin.clear();
+      while (cin.get() != '\n');
+      cout << "Invalid input. Please enter a number between 1 and 5." << endl;
     }
-     else{
-       cin.clear();
-       cin.ignore();
-       isInt = false;
-       cout << "Invalid input. Please enter a number." << endl;
-       cin >> choice;
-       }
-  choice = static_cast<choiceNum>(choice);
-  cout << "Your choice: " << choice << endl;
-
-    //switch case for game options
-    switch (choice){
-      //display the left number
-      case displayLeft:
-        //keeping user from seeing both numbers
-        if(num1 == -1 && num2 == -1){
-          num1 = left;
-          cout << "You will only get one point for guessing correctly and lose ten points for guessing incorrectly, now." << endl;
-          cout << num1 << "  " << num2 << endl;
-        }
-          //stop them from seeing both numbers
-        else{
+    
+    //making choice valid for the enum
+    choice = static_cast<choiceNum>(choice);
+    cout << "Your choice: " << choice << endl;
+    
+    // switch case for game options
+    switch (choice) {
+    // display the left number
+    case displayLeft:
+      if (num1 == -1 && num2 == -1) {
+        num1 = left;
+        cout << "You will only get one point for guessing correctly and lose "
+                "ten points for guessing incorrectly, now."
+             << endl;
+        cout << num1 << "  " << num2 << endl;
+      }
+      // stop them from seeing both numbers
+        else {
           cout << "You have already displayed the a number. You cannot display both" << endl;
           cout << num1 << "  " << num2 << endl;
         }
       break;
-      //display the right number
-      case displayRight:
-        if(num2 == -1 && num1 == -1){
-          num2 = right;
-          cout << "You will only get one point for guessing correctly and lose ten points for guessing incorrectly, now." << endl;
-          cout << num1 << "  " << num2 << endl;
-        }
-          //stop them from seeing both numbers
-        else{
-          cout << "You have already displayed the a number. You cannot display both" << endl;
-          cout << num1 << "  " << num2 << endl;
-        }
+    // display the right number
+    case displayRight:
+      if (num2 == -1 && num1 == -1) {
+        num2 = right;
+        cout << "You will only get one point for guessing correctly and lose "
+                "ten points for guessing incorrectly, now."
+             << endl;
+        cout << num1 << "  " << num2 << endl;
+      }
+      // stop them from seeing both numbers
+        else {
+         cout << "You have already displayed the a number. You cannot display both" << endl;
+         cout << num1 << "  " << num2 << endl;
+        } 
       break;
-      // let them guess the number
-      case guess:
-        int guess;
-        cout << "Enter your guess: " << endl;
-        cin >> guess;
-        //Checking to make sure it's a correct guess. If true add points.
-        if(guess >= left && guess <= right){
-          cout << "You guessed correctly!" << endl;
-          // check if they got a cheat on one of the numbers if not add 5 and print out points.
-          if(num1 == -1 && num2 == -1){
-            points += 5;
-            cout << "You get five points!" << endl;
-            cout << "Your total points: " << points << endl;
-          }
-          // if they used a cheat then only add 1 point and print out total points.
-          else{
-            points += 1;
-            cout << "You get one points!" << endl;
-            cout << "Your total points: " << points << endl;
-          }
+    // let them guess the number
+    case guess:
+      int guess;
+      cout << "Enter your guess: " << endl;
+      cin >> guess;
+      // Checking to make sure it's a correct guess. If true add points.
+      if (guess >= left && guess <= right) {
+        cout << "You guessed correctly!" << endl;
+        // adding points if user did not display either number and printing total points
+        if (num1 == -1 && num2 == -1) {
+          points += 5;
+          cout << "You get five points!" << endl;
+          cout << "Your total points: " << points << endl;
         }
-          // tell them they missed and deduct points accordingly and printout the total points.
-        else{
+          // adding a point if user did display either number and printing total points
+          else {
+           points += 1;
+           cout << "You get one points!" << endl;
+           cout << "Your total points: " << points << endl;
+          }
+      }
+      // user guessed incorrectly
+        else {
           cout << "You guessed incorrectly!" << endl;
-          if(num1 == -1 && num2 == -1){
+          // subtracting points if user did not display either number and printing total points
+          if (num1 == -1 && num2 == -1) {
             points -= 5;
             cout << "You lose five points" << endl;
-            cout << "Your total points: " << points << endl;
-          }
-          else{
+           cout << "Your total points: " << points << endl;
+          } 
+            else {
+            // subtracting 10 points if user did display either number and printing total points
             points -= 10;
             cout << "You lose ten points!" << endl;
             cout << "Your total points: " << points << endl;
-          }
+           }
         }
       break;
-      case change:
-      //giving left and right new random numbers and keeping left below right
+    case change:
+      // giving left and right new random numbers and keeping left below right
       left = rand() % 101 + 150;
       right = rand() % 101 + 150;
-      while(left >= right){
+      while (left >= right) {
         left = rand() % 101 + 150;
-      }
+      } 
+      //reassigning num1 and num2 to -1 to reset them
       num1 = -1;
       num2 = -1;
       points -= 1;
@@ -174,19 +188,17 @@ int main() {
       cout << "Generating new numbers..." << endl;
       cout << num1 << "  " << num2 << endl;
       break;
-      // let them leave the game option and tell them their total points.
-      case exit:
-        cout << "Bye " << name << "!!!" << endl;
-        cout << "Your final points balance is: " << points << endl;
+    // let them leave the game  and tell them their total points.
+    case exit:
+      cout << "Bye " << name << "!!!" << endl;
+      cout << "Your final points balance is: " << points << endl;
       break;
-      default:
-      //in case user enters a number that is not an option
-        cout << "Invalid choice. Press enter to continue" << endl;
+    default:
+      // in case user input is not an option
+      cout << "Invalid choice. Press enter to continue" << endl;
       break;
     }
-  }
-  //breaks the loop when user enters 5 or points fall below 0
-  while(choice != 5 && points >= 0);
-  
-  return 0;
+  } while (choice != 5 && points >= 0); //breaks the loop if user exits or runs out of points
+
+return 0;
 }
